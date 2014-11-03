78a1a900c8fb8e9a4de38d46513925120e466e9b
Matias Tailanian <matias@tailanian.com>
2014-11-03 02:45:27 +0100

    tidier? maybe

--- PGM/2/src/EM.m
+++ PGM/2/src/EM.m
@@ -14,26 +14,22 @@ mu      = pgm_kMeans(x, c, opt)';
 %mu=ones(4,2);
 t       = 1;
 epsilon = 1e-07;
-Pi      = (1/4)*ones(c,1);
+Pi      = (1/c)*ones(c,1);
 SIGMA   = zeros(d,d,c);
 
 for j=1:c
-    SIGMA(:,:,j)=eye(d,d);
+    SIGMA(:,:,j)=eye(d);
 end
 
-%p_z_x=compute_p_z_x(x,Pi,mu,SIGMA);
-%l=computeLikelyhood(p_z_x,x,Pi,mu,SIGMA);
-
-counter = 1;
-while (1)
+l        = -inf;
+lDiff    = inf;
+counter  = 1;
+while (lDiff > epsilon)
+    
     % Given theta, with bayes formula compute p(z|x)=q* (partial maximisation)
-    p_z_x = compute_p_z_x(x,Pi,mu,SIGMA);
-    
-    l_new=computeLikelyhood(p_z_x,x,Pi,mu,SIGMA)
-    
-    if ( counter > 1 && abs(l-l_new) < epsilon )
-        break;
-    end
+    p_z_x = compute_p_z_x(x,Pi,mu,SIGMA);   
+    l_new = computeLikelyhood(p_z_x,x,Pi,mu,SIGMA);
+    lDiff = abs(l-l_new);
     
     %closed formula for updating the theta variables given p(z|x)
     for j=1:c
@@ -56,9 +52,9 @@ while (1)
             SIGMA(:,:,j) = SIGMA(:,:,j) + (p_z_x(i,j))*(x(:,i)'-mu(j,:))'*(x(:,i)'-mu(j,:));
         end
         
-        SIGMA(:,:,j)= SIGMA(:,:,j)./sum(p_z_x(:,j));
+        SIGMA(:,:,j) = SIGMA(:,:,j)./sum(p_z_x(:,j));
     end
     
-    l=l_new;
-    counter=counter+1;
+    l       = l_new;
+    counter = counter+1;
 end
--- PGM/2/src/compute_p_z_x.m
+++ PGM/2/src/compute_p_z_x.m
@@ -1,15 +1,23 @@
-function p_z_x=compute_p_z_x(x,Pi,mu,sigma)
+function p_z_x=compute_p_z_x(x,Pi,mu,SIGMA)
+% -------------------------------------------------------------------------
+% function p_z_x=compute_p_z_x(x,Pi,mu,SIGMA)
+% -------------------------------------------------------------------------
+% inputs:
+%   - x:  matrix dxN of samples
+%   - Pi: vector cx1
+%   - mu: matrix cxd of means of clusters
+% output:
+%   - p_z_x
+% -------------------------------------------------------------------------
 
-%sigma is a vector of covariance matrices
-%mu a vector of means
-n=size(x,2);
-c=size(mu,1);
-p_z_x=zeros(n,c);
-for i=1:n
-    for j=1:c
-        p_z_x(i,j)=Pi(j)*Normale(x(:,i),mu(j,:),sigma(:,:,j));
+[d,N] = size(x);
+c     = size(mu,1);
+pdf   = @(x,mu,SIGMA) exp(-0.5*(x-mu)'*(SIGMA\(x-mu)))/sqrt(det(SIGMA)*(2*pi)^d);
+p_z_x = zeros(N,c);
+
+for j=1:c
+    for i=1:N
+        p_z_x(i,j) = Pi(j)*pdf(x(:,i),mu(j,:)',SIGMA(:,:,j));
     end
-    p_z_x(i,:)=p_z_x(i,:)./sum(p_z_x(i,:));
 end
-
-end
+p_z_x = p_z_x ./ repmat(sum(p_z_x,2),1,c);
