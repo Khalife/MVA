b9833589656906d6e808dcf938e8f328edc0e3c0
Khalife <sam_411@hotmail.fr>
2014-11-06 17:31:22 +0100

    computeLikelyHood Mistake

--- PGM/2/src/pgm_computeLikelyhood.m
+++ PGM/2/src/pgm_computeLikelyhood.m
@@ -1,9 +1,9 @@
 function l=pgm_computeLikelyhood(p_z_x,x,Pi,mu,SIGMA)
 
 l=0;
-for i=1:size(x,1)
+for i=1:size(x,2)
     for j=1:size(p_z_x,2)
-        l=l+p_z_x(i,j)*(log(Pi(i))-log(2*pi)-0.5*log(det(SIGMA(:,:,j)))-0.5*(x(:,i)'-mu(j,:))*(SIGMA(:,:,j)\(x(:,i)'-mu(j,:))'));
+        l=l+p_z_x(i,j)*(log(Pi(j))-log(2*pi)-0.5*log(det(SIGMA(:,:,j)))-0.5*(x(:,i)'-mu(j,:))*(SIGMA(:,:,j)\(x(:,i)'-mu(j,:))'));
     end
 end
 
